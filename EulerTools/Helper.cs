3ad650fb888f74bc58a7547df4017936c230ddc7
bondeluke <bondeluke@gmail.com>
2014-06-02 10:55:51 -0500

    Completed First Problem
    I completed the first problem and added the EulerTools Namespace

---
+++ EulerTools/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerTools
+{
+    public static class EulerHelper
+    {
+        public static void DisplayAnswer(int number)
+        {
+            Console.WriteLine(number);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MultiplesOf3And5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EulerTools;
+
+namespace MultipliesOf3And5
+{
+    // Problem 1
+    // Find the sum of all the multiples of 3 or 5 below 1000. 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int number = 0; number < 1000; number++)
+                if (IsMultipleOf3Or5(number))
+                    sum += number;
+
+            EulerHelper.DisplayAnswer(sum);
+        }
+
+        static bool IsMultipleOf3Or5(int number)
+        {
+            if (number % 3 == 0 || number % 5 == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
