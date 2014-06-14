bb05332702af2d29f16d4931f9ae85a9cd1a1660
bondeluke <bondeluke@gmail.com>
2014-06-14 17:07:10 -0500

    Completed Second Problem
    Even Fibonacci Numbers

---
+++ EvenFibonacci/FibGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenFibonacci
+{
+    class FibGenerator
+    {
+        private int currentFib;
+        private int prevFib;
+
+        public FibGenerator()
+        {
+            currentFib = 0;
+            prevFib = 1;
+        }
+
+        public int Next()
+        {
+            int nextFib = currentFib + prevFib;
+            prevFib = currentFib;
+            currentFib = nextFib;
+            return nextFib;
+        }
+    }
+}
---
+++ EvenFibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using EulerTools;
+using System.Collections;
+
+namespace EvenFibonacci
+{
+    class Program
+    {
+        const int limit = 4000000;
+
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int fib;
+            FibGenerator fibGenerator = new FibGenerator();
+
+            while ((fib = fibGenerator.Next()) < limit)
+                if (IsEven(fib))
+                    sum += fib;
+            
+            EulerHelper.DisplayAnswer(sum);
+        }
+
+        static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
