25656051aecf2e54f9c42e915f3420868af30918
bondeluke <bondeluke@gmail.com>
2014-07-01 23:24:59 -0500

    Experimenting #4
    Playing around to see if I can list products in descending order.

---
+++ LargestPalindromeProduct/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestPalindromeProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int smallerHalf, largerHalf, product, previousProduct = Int32.MaxValue;
+            for (int sum = 1998; sum > 200; sum--)
+            {
+                smallerHalf = sum / 2;
+                largerHalf = sum - smallerHalf;
+                while (largerHalf <= 999 && smallerHalf >= 100)
+                {
+                    product = smallerHalf * largerHalf;
+                    Console.WriteLine(String.Format("{0} x {1} = {2}", largerHalf++, smallerHalf--, product));
+                    if (previousProduct < product)
+                        break;
+                    previousProduct = product;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
